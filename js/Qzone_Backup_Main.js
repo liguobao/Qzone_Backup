dc35522d31ed087f9fcd13cb2d484aa3ec0ebca9
liguobao <codelover@qq.com>
2017-03-14 23:40:51 +0800

    异步send问题暂未解决

--- js/Qzone_Backup_Main.js
+++ js/Qzone_Backup_Main.js
@@ -1,9 +1,31 @@
 var msgList ;
 var shuoshuoInfoList = [];
+var pagerCount = 0;
 
 $("#btnGetShuoShuo").on("click",function(){
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendRequest(tab.id, {method: "msgList"}, function(response) {
+
+    
+    chrome.tabs.sendRequest(tab.id, {method: "pager_last_0"}, function(response) {
+          if(response.method=="pager_last_0"){
+              pagerCount =  response.data;    
+              console.log("当前页数："+pagerCount);
+              for(var index =1;index<5;index++){
+                  chrome.tabs.getSelected(null, function(tab) {
+                        chrome.tabs.sendRequest(tab.id, {method: "setPagerGoCount",data:index}, function(response) {
+                             if(response.method=="setPagerGoCount"){
+                                  $("#btnGetShuoShuo").html("<i class='am-icon-spinner am-icon-spin'></i>正在跳转至第"+index +"页");
+                                 console.log(response.data);
+                                }
+                            });});
+                 
+              }
+          }
+     });
+
+
+/*    chrome.tabs.sendRequest(tab.id, {method: "msgList"}, function(response) {
+        $("#btnGetShuoShuo").html("<i class='am-icon-spinner am-icon-spin'></i>正在获取数据...");
         if(response.method=="msgList"){
             msgList = $(response.data);
             msgList.each(function(index){
@@ -20,6 +42,27 @@ $("#btnGetShuoShuo").on("click",function(){
                shuoshuoInfoList.push(shuoshuoInfo);
             });
         }
-    });
+        if(shuoshuoInfoList.length>0){
+            $("#btnGetShuoShuo").html("获取数据成功！");
+        }else{
+            $("#btnGetShuoShuo").html("获取数据失败！");
+        }
+        
+    });*/
+/*chrome.tabs.getSelected(null, function(tab) {
+     chrome.tabs.sendRequest(tab.id, {method: "pager_last_0"}, function(response) {
+          if(response.method=="pager_last_0"){
+              pagerCount =  response.data;    
+              console.log(pagerCount);          
+          }
+     });});
+
+chrome.tabs.getSelected(null, function(tab) {
+     chrome.tabs.sendRequest(tab.id, {method: "setPagerGoCount",data:10}, function(response) {
+          if(response.method=="setPagerGoCount"){
+              console.log( response.data);          
+          }
+     });});*/
+
 });
 });
--- myscript.js
+++ myscript.js
@@ -1,10 +1,40 @@
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
       if(request.method=="msgList"){
-            var msgList= $(window.frames["app_canvas_frame"].document).find("#msgList");
+            var msgListLabel= window.frames["app_canvas_frame"]!=undefined ? $(window.frames["app_canvas_frame"].document).find("#msgList"):$(".app_canvas_frame").contents().find("#msgList");
             // var msgList= $(".app_canvas_frame").contents().find("#msgList");
-            console.log(msgList);
-             sendResponse({data: msgList[0].innerHTML, method: "msgList"}); //same as innerText
+            console.log("msgList 信息："+ msgListLabel);
+            var msgListText = undefined;
+            if(msgListLabel==undefined){
+                console.log("获取数据失败...");
+            }else{
+               msgListText=  msgListLabel[0].innerHTML;
+            }
+            sendResponse({data: msgListText, method: "msgList"}); //same as innerText
+        }else if(request.method=="pager_last_0"){
+            var pager_last_0Lable= window.frames["app_canvas_frame"]!=undefined ? $(window.frames["app_canvas_frame"].document).find("#pager_last_0")
+            :$(".app_canvas_frame").contents().find("#pager_last_0");
+            var pagerCount = undefined;
+            if(pager_last_0Lable!=undefined){
+                pagerCount = $(pager_last_0Lable).find("span")[0].innerHTML
+            }
+            sendResponse({data: pagerCount, method: "pager_last_0"}); //same as innerText
+        }else if(request.method=="setPagerGoCount"){
+            var pager_go_0 = window.frames["app_canvas_frame"]!=undefined ? $(window.frames["app_canvas_frame"].document).find("#pager_go_0")
+            :$(".app_canvas_frame").contents().find("#pager_go_0");
+
+            var pager_gobtn_0 = window.frames["app_canvas_frame"]!=undefined ? $(window.frames["app_canvas_frame"].document).find("#pager_gobtn_0")
+            :$(".app_canvas_frame").contents().find("#pager_gobtn_0");
+            if(pager_go_0&&pager_gobtn_0){
+                $(pager_go_0[0]).val(request.data);
+                $(pager_gobtn_0[0]).click();
+                window.scrollTo(0,document.body.scrollHeight);
+                window.scrollTo(0,document.body.scrollHeight);
+                sendResponse({data: "success", method: "setPagerGoCount"}); //same as innerText
+            }else{
+                sendResponse({data: "fail", method: "setPagerGoCount"}); //same as innerText
+            }
+           
         }
     }
 );
