5207342f18a4249893bfdd30a3eff70eab4923ec
ligb <ligb@Ctrip.com>
2017-03-14 13:35:42 +0800

    finish msg list

--- js/Qzone_Backup_Main.js
+++ js/Qzone_Backup_Main.js
@@ -5,20 +5,20 @@ $("#btnGetShuoShuo").on("click",function(){
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendRequest(tab.id, {method: "msgList"}, function(response) {
         if(response.method=="msgList"){
-            msgList = response.data;
-            $(msgList).each(function(li){
-                var content =$(li).find(".bd")[0];
-                var customtail = $(li).find(".custom-tail")[0];
-                var date =$(($li).find(".goDetail")[0]).attr("title");
-                 var shuoshuoInfo = {
+            msgList = $(response.data);
+            msgList.each(function(index){
+                var li = msgList[index];
+                var content =$($(li).find(".bd")[0]).find("pre")[0].innerHTML;
+                var customtail =$($($(li).find("span.custom-tail"))[0]).attr("title");
+                var date =$($($(li).find("a.goDetail"))[0]).attr("title");
+                var shuoshuoInfo = {
                      "content":content,
                      "date":date,
-                     "customtail":customtail
+                     "customtail":customtail!=undefined && customtail!=""?customtail:"Web",
                     };
+               console.log(shuoshuoInfo);
+               shuoshuoInfoList.push(shuoshuoInfo);
             });
-            
-
-
         }
     });
 });
--- myscript.js
+++ myscript.js
@@ -2,6 +2,7 @@ chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
       if(request.method=="msgList"){
             var msgList= $(window.frames["app_canvas_frame"].document).find("#msgList");
+            // var msgList= $(".app_canvas_frame").contents().find("#msgList");
             console.log(msgList);
              sendResponse({data: msgList[0].innerHTML, method: "msgList"}); //same as innerText
         }
