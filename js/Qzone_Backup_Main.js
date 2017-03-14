6dee0dd70064b54028c360f17fa30f58c92132ae
liguobao <codelover@qq.com>
2017-03-14 09:00:02 +0800

    遍历数据

--- js/Qzone_Backup_Main.js
+++ js/Qzone_Backup_Main.js
@@ -1,12 +1,24 @@
 var msgList ;
+var shuoshuoInfoList = [];
 
 $("#btnGetShuoShuo").on("click",function(){
   chrome.tabs.getSelected(null, function(tab) {
     chrome.tabs.sendRequest(tab.id, {method: "msgList"}, function(response) {
         if(response.method=="msgList"){
             msgList = response.data;
-            console.log(msgList);
-            $($(msgList)[5]).find(".bd")[0];
+            $(msgList).each(function(li){
+                var content =$(li).find(".bd")[0];
+                var customtail = $(li).find(".custom-tail")[0];
+                var date =$(($li).find(".goDetail")[0]).attr("title");
+                 var shuoshuoInfo = {
+                     "content":content,
+                     "date":date,
+                     "customtail":customtail
+                    };
+            });
+            
+
+
         }
     });
 });
